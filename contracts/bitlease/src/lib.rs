4943e2321c30d5ce8bf4e0c9e4bc9ba912b9c858
pieri93 <pierinaponce1993@gmail.com>
2023-04-15 21:51:41 -0300

    creates enum for accepted currency

--- contracts/bitlease/src/lib.rs
+++ contracts/bitlease/src/lib.rs
@@ -15,5 +15,28 @@ mod bitlease_contract {
         currency_to_invest: Currency,
     }
 
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, serde::Serialize, serde::Deserialize))]
+    pub enum Currency {
+        BITCOIN, 
+        ETHEREUM,
+        TETHER,
+    }
+
     
 }
+
+
+    
+/*
+For eg. When a lender has a deposit to make, it should go to a pool
+function lend(lenderid, currency, amount)
+function borrow(borrowerid, downpaymentCurrency, downpaymentAmount,  borrowCurrency, borrowAmount, interest_rate) 
+We need a few hashmaps that store the things as key value
+
+calculateInterest function
+
+lender's amount goes to the total pool.. borrower borrors form the total pool
+that pool is a hashmap or multiple hashmaps that tracks the amounts for ecah borrowers and lenders
+
+*/
